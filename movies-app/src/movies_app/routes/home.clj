4757e256fe78cb037e4952b3f49a47585b517fee
tasica <atasic90@gmail.com>
2015-01-10 02:46:32 +0100

    search functionality

--- movies-app/src/movies_app/routes/home.clj
+++ movies-app/src/movies_app/routes/home.clj
@@ -1,23 +1,53 @@
 (ns movies-app.routes.home
-(:require [compojure.core :refer :all]
-[movies-app.views.layout :as layout]
-[hiccup.form :refer :all]))
+  (:require [compojure.core :refer :all]
+            [movies-app.views.layout :as layout]
+            [movies-app.util.extract :as util] 
+            [noir.response :refer [redirect]]
+            [hiccup.form :refer :all]))
 
-(defn home []
+(defn home [movies search-term]
   (layout/common 
     [:h1 "Movies"]
-[:p "Welcome to my guestbook"]
-[:hr]
-[:form
-[:p "Name:"]
-[:input]
-[:p "Message:"]
-[:textarea {:rows 10 :cols 40}]]))
+    [:p "Welcome to powerful Tomatoer"]
+    [:hr]
+    (form-to [:post "/search"] 
+             (if-not (nil? search-term)
+              (text-field {:value search-term} :search-term)
+              (text-field {:placeholder "Enter search term"} :search-term))
+              (submit-button "Search"))
+    (if-not (nil? movies)
+      (list-of-movies movies)
+      )
+    )
+  
+  )
 
-(defroutes home-routes
-  (GET "/home" [] (home)))
 
+(defn list-of-movies [movies]
+   (for [movie movies]
+    (let [ratings (:ratings movie)]
+    [:ul
+     [:li (:title movie)]
+     ])))
+  
+  (defn search-for-movies [search-term]
+    (println "Searching for movies..." search-term)
+    (let [links-url (util/get-links-url)
+          template-url (util/get-template-url links-url)
+          movie-url (util/get-movies-url template-url search-term)
+          movies-data (util/get-movies-data movie-url)]
+    (home movies-data search-term)))
 
+  
 
-
-
+  
+  (defroutes home-routes
+    (GET "/home" [] (home nil nil))
+    (GET "/home" [movies] (home movies))
+    (POST "/search" [search-term] (search-for-movies search-term)))
+  
+  
+  
+  
+  
+  
---
+++ movies-app/src/movies_app/util/extract.clj
@@ -0,0 +1,43 @@
+(ns movies-app.util.extract
+    (:require
+   [clojure.string :as cs]
+   [clj-http.client :as client]
+   [cheshire.core :as cheshire]))
+
+(import [java.net URLEncoder])
+
+(defn get-api-key []
+  (str "cfu7ks8sbc9yur4sts9zayyx")
+  )
+
+(defn scoop-url [url]
+  (let [r (client/get url)
+        code (:status r)
+        body (:body r)]
+    [code body]))
+
+(defn get-links-url []
+  (let [[code body] (scoop-url (str "http://api.rottentomatoes.com/api/public/v1.0.json?apikey=" (get-api-key)))]
+    (when (= code 200) 
+      (:movies (:links (cheshire/parse-string body true))))))
+
+(defn get-template-url [links-url]
+  (println (str links-url "?apikey=" (get-api-key)))
+  (let [[code body] (scoop-url (str links-url "?apikey=" (get-api-key)))]
+    (when (= code 200) 
+      (:link_template (cheshire/parse-string body true)))))
+
+(defn get-movies-url [template search-term]
+  (println template)
+  (let [search (cs/replace template "{search-term}" (URLEncoder/encode search-term))
+        results (cs/replace search "{results-per-page}" "50")
+        page (cs/replace results "{page-number}" "1")
+        fin (str page "&apikey=" (get-api-key))]
+    fin) 
+  )
+
+(defn get-movies-data [movie-url]
+  (let [[code body] (scoop-url movie-url)]
+    (if (= code 200)
+      (:movies (cheshire/parse-string body true))     
+      nil)))
