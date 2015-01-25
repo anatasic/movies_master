67fff5d81a47e001397cef6be04677dc003edb3a
tasica <atasic90@gmail.com>
2015-01-25 01:47:11 +0100

    similar movies

---
+++ movies-app/src/movies_app/routes/favorites.clj
@@ -0,0 +1,18 @@
+(ns movies-app.routes.favorites
+  (:require [compojure.core :refer :all]
+            [movies-app.views.layout :as layout]
+            [movies-app.util.extract :as util]
+            [hiccup.element :refer :all]
+            [hiccup.core :refer :all]
+            [clojure.string :as cs]
+            [hiccup.form :refer :all]))
+  
+  (defn favorites
+    (layout/common
+      [:h2 "Your favorite movies"]
+      )
+    )
+  
+  (defroutes favorites-routes
+    (POST "/favorite" [username movie] (favorites))
+    )
